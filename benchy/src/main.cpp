25cc395d93dafe3f0e59ccc492a0e25b6cf656b5
huangw4 <73304411+huangw4@users.noreply.github.com>
2023-10-23 17:03:22 -0400

    stepperpractice

--- benchy/src/main.cpp
+++ benchy/src/main.cpp
@@ -2,6 +2,10 @@
 #include <Arduino_FreeRTOS.h>
 
 #define ACTIVITY_LED 13
+#define STEPPER_IN1 11
+#define STEPPER_IN2 10
+#define STEPPER_IN3 9
+#define STEPPER_IN4 8
 
 // put function declarations (prototypes) here:
 
@@ -9,6 +13,7 @@
 // for what a task needs to do.
 void BlinkTaskPractice(void *blinkParameters);
 void SerialPrintTaskPractice(void *printParams);
+void StepperMotorPractice(void *stepperParams);
 
 // Global variables:
 TaskHandle_t * blinkHandle;
@@ -17,9 +22,11 @@ void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   
+  
+  BaseType_t stepperSuccess = xTaskCreate(StepperMotorPractice,"step", 512, nullptr, 2, nullptr);
   BaseType_t blinkSuccess = xTaskCreate(BlinkTaskPractice,"Blink", 512, nullptr, 1, blinkHandle);
 
-  if(blinkSuccess == pdPASS) {
+  if(stepperSuccess == pdPASS) {
     String out = String("Hello, Blink: ");
     Serial.println(out);
   } else {
@@ -44,11 +51,84 @@ void BlinkTaskPractice(void *blinkParameters) {
     state = state == 1 ? 0 : 1;
     
     // Delay => According to Arduino_FreeRTOS docs, delay calls vTaskDelay automatically.
-    delay(500);
+    delay(5000);
   }
 
   // in the event the for loop exits:
   vTaskDelete(NULL);
+
+}
+
+void StepperMotorPractice(void *stepperParams) {
+  //Setup
+  pinMode(STEPPER_IN1, OUTPUT);
+  pinMode(STEPPER_IN2, OUTPUT);
+  pinMode(STEPPER_IN3, OUTPUT);
+  pinMode(STEPPER_IN4, OUTPUT);
+  BaseType_t state = 0;
+
+  while(true){
+    if(state>7){
+      state=0;
+    }
+    switch(state){
+    case 0: 
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,0);
+      digitalWrite(STEPPER_IN3,0);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    case 1:
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,0);
+      digitalWrite(STEPPER_IN3,0);
+      digitalWrite(STEPPER_IN4,1);
+      
+      break;
+    case 2: 
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,0);
+      digitalWrite(STEPPER_IN3,1);
+      digitalWrite(STEPPER_IN4,1);
+      break;
+    case 3: 
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,0);
+      digitalWrite(STEPPER_IN3,1);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    case 4: 
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,1);
+      digitalWrite(STEPPER_IN3,1);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    case 5: 
+      digitalWrite(STEPPER_IN1,0);
+      digitalWrite(STEPPER_IN2,1);
+      digitalWrite(STEPPER_IN3,0);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    case 6: 
+      digitalWrite(STEPPER_IN1,1);
+      digitalWrite(STEPPER_IN2,1);
+      digitalWrite(STEPPER_IN3,0);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    case 7: 
+      digitalWrite(STEPPER_IN1,1);
+      digitalWrite(STEPPER_IN2,0);
+      digitalWrite(STEPPER_IN3,0);
+      digitalWrite(STEPPER_IN4,0);
+      break;
+    default: state = 0;
+    }
+    state++;
+    Serial.println(state);
+    delay(10);
+  }
+
+
 
 }
 
