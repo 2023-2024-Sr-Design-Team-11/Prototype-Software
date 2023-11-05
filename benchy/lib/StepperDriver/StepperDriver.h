d32fd15c43fe843830c9bdbb5edff7685f442aff
huangw4 <73304411+huangw4@users.noreply.github.com>
2023-11-05 14:52:28 -0500

    StepperDriver

---
+++ benchy/lib/StepperDriver/StepperDriver.h
@@ -0,0 +1,13 @@
+// StepperDriver.h
+
+#ifndef StepperDriver_H
+#define StepperDriver_H
+
+class StepperMotor{};
+void StartMotor(StepperMotor motor){};
+#define CLOCKWISE 0
+#define COUNTERCLOCKWISE 1
+
+#endif
+
+
